cc292d1b06a307ae2005cd7058fae63d1f06d859
Sudarsan Surendralal <sudarsan.sl@gmail.com>
2021-05-13 17:31:45 +0200

    Create run_notebooks.sh

---
+++ .ci-support/run_notebooks.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Select ipykernelt
+if [ "$PYTHONVER" = "2.7" ]; then
+    kernel="python2"
+else
+    kernel="python3"
+fi;
+
+i=0;
+for notebook in $(ls notebooks/*.ipynb); do 
+    papermill ${notebook} ${notebook%.*}-out.${notebook##*.} -k $kernel || i=$((i+1));
+done;
+
+# push error to next level
+if [ $i -gt 0 ]; then
+    exit 1;
+fi;
